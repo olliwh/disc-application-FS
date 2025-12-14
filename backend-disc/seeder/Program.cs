083ee8d3911c2dff244393aa28f94e73075fc8af
ollwh <98967791+olliwh@users.noreply.github.com>
2025-12-14 14:15:25 +0100

    last commit

--- backend-disc/seeder/Program.cs
+++ backend-disc/seeder/Program.cs
@@ -25,6 +25,7 @@ namespace seeder
                 string sqlCreateStoredProcEdit = await ReadSqlFile("createStoredProcEdit.sql");
                 string sqlCreateView = await ReadSqlFile("createView.sql");
                 string sqlInsertData = await ReadSqlFile("insertDataQuery.sql");
+                string sqlCreateIndex = await ReadSqlFile("createIndex.sql");
 
                 await WaitForTable(connectionString, "dbo.stress_measures");
                 await WaitForTable(connectionString, "dbo.employees");
@@ -38,6 +39,7 @@ namespace seeder
                     await ExecuteNonQuery(connectionString, sqlCreateStoredProcEdit);
                     await ExecuteNonQuery(connectionString, sqlCreateView);
                     await ExecuteNonQuery(connectionString, sqlInsertData);
+                    await ExecuteNonQuery(connectionString, sqlCreateIndex);
                     Console.WriteLine("Seeder completed successfully!");
                 }
             }
