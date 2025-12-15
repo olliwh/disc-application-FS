70e0031447f259a7abf5c04259f90c043fe6a171
ollwh <98967791+olliwh@users.noreply.github.com>
2025-12-15 05:31:57 +0100

    updated sql

--- DatabaseData/newDb.sql
+++ DatabaseData/newDb.sql
@@ -119,7 +119,6 @@ CREATE TABLE project_tasks (
     completed BIT NOT NULL,
     time_of_completion DATETIME,
     time_to_complete_id INT,
-    evaluation NVARCHAR(255),
     project_id INT NOT NULL,
     FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
     FOREIGN KEY (time_to_complete_id) REFERENCES completion_intervals(id)
@@ -176,6 +175,21 @@ CREATE INDEX IX_users_username ON users(username);
 CREATE INDEX IX_employees_phone ON employees(work_phone);
 CREATE INDEX IX_employees_first_name ON employees(first_name);
 CREATE INDEX IX_employees_last_name ON employees(last_name);
+CREATE INDEX IX_project_tasks_project_id ON project_tasks(project_id);
+CREATE INDEX IX_project_tasks_time_to_complete_id ON project_tasks(time_to_complete_id);
+CREATE INDEX IX_stress_measures_employee_id ON stress_measures(employee_id);
+CREATE INDEX IX_stress_measures_task_id ON stress_measures(task_id);
+CREATE INDEX IX_project_tasks_employees_task_id ON project_tasks_employees(task_id);
+CREATE INDEX IX_project_tasks_employees_employee_id ON project_tasks_employees(employee_id);
+CREATE INDEX IX_employees_projects_project_id ON employees_projects(project_id);
+CREATE INDEX IX_employees_projects_employee_id ON employees_projects(employee_id);
+CREATE INDEX IX_projects_disc_profiles_project_id ON projects_disc_profiles(project_id);
+CREATE INDEX IX_projects_disc_profiles_disc_profile_id ON projects_disc_profiles(disc_profile_id);
+CREATE INDEX IX_users_user_role_id ON users(user_role_id);
+CREATE INDEX IX_employee_private_data_employee_id ON employee_private_data(employee_id);
+
+-- Projects Audit foreign key
+CREATE INDEX IX_projects_audit_project_id ON projects_audit(project_id);
 GO
 CREATE OR ALTER TRIGGER add_to_project_audit_table ON projects
 AFTER INSERT
@@ -263,23 +277,14 @@ GO
 -- Stored procesdure
 DROP PROCEDURE IF EXISTS sp_AddEmployee;
 GO
-use disc_profile_relational_db;
-go
 CREATE OR ALTER PROCEDURE sp_AddEmployee
-    @first_name NVARCHAR(255),
-    @last_name NVARCHAR(255),
-    @work_email NVARCHAR(255),
-    @work_phone VARCHAR(25) = NULL,
-    @image_path VARCHAR(255),
-    @department_id INT,
-    @position_id INT = NULL,
-    @disc_profile_id INT = NULL,
-    @cpr CHAR(10),
-    @private_email NVARCHAR(255),
-    @private_phone VARCHAR(25),
-    @username NVARCHAR(64),
-    @password_hash VARCHAR(255),
-    @user_role_id INT
+    @first_name NVARCHAR(255), @last_name NVARCHAR(255),
+    @work_email NVARCHAR(255), @work_phone VARCHAR(25) = NULL,
+    @image_path VARCHAR(255), @department_id INT,
+    @position_id INT = NULL, @disc_profile_id INT = NULL,
+    @cpr CHAR(10), @private_email NVARCHAR(255),
+    @private_phone VARCHAR(25), @username NVARCHAR(64),
+    @password_hash VARCHAR(255), @user_role_id INT
 AS
 BEGIN
     BEGIN TRY
@@ -321,10 +326,31 @@ BEGIN
         THROW 50001, 'Employee not found', 1;
 END;
 GO
+DROP PROCEDURE IF EXISTS sp_LoginAsUser;
+GO
+CREATE PROCEDURE sp_LoginAsUser
+    @username NVARCHAR(64),
+    @password VARCHAR(255)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF EXISTS (SELECT 1 FROM users WHERE username = @username AND password_hash = @password)
+    BEGIN
+        SELECT username, employee_id, user_role_id
+        FROM users
+        WHERE username = @username AND password_hash = @password;
+    END
+    ELSE
+    BEGIN
+        THROW 50001, 'User not found or invalid credentials', 1;
+    END
+END;
+GO
 
 
 --CREATE USER testUser WITHOUT LOGIN;
-GRANT SELECT ON employee_private_data TO testUser;
+--GRANT SELECT ON employee_private_data TO testUser;
 
 PRINT 'Database schema created successfully!';
 GO
@@ -363,7 +389,7 @@ INSERT INTO user_roles(name, description) VALUES
 
 
 --    first_name  last_name work_email work_phone image_path, department_id position_id disc_profile_id cpr CHAR(10) private_email private_phone username password_hash user_role_id INT
-EXEC sp_AddEmployee 'Admin', 'Admin', 'Admin@techcorp.com', '88888927', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 1, 1, 1, '1704867890', 'admin@mail.com', '12345678', 'admin', '$argon2id$v=19$m=65536,t=3,p=1$uclpX8S5LhZgLBTruwFXmQ$UQZjeO+ziT58SUvHLie5SLZVI5h9jPqUM+BxXvIzlfA', 1;
+EXEC sp_AddEmployee 'Admin', 'Admin', 'Admin@techcorp.com', '88888927', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 4, 1, 1, '1704867890', 'admin@mail.com', '12345678', 'admin', '$argon2id$v=19$m=65536,t=3,p=1$uclpX8S5LhZgLBTruwFXmQ$UQZjeO+ziT58SUvHLie5SLZVI5h9jPqUM+BxXvIzlfA', 1;
 
 EXEC sp_AddEmployee 'Alice', 'Jensen', 'alice@techcorp.com', '88887777', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 1, 1, 1, '1234567890', 'alice@mail.com', '12328678', 'alice', '$argon2id$v=19$m=65536,t=3,p=1$RwVS0w4lmni/7EQRa0P2yg$587TA/40h5IAI76xmRvGEiMgcp+PWr+sTamD50pAy5g', 3;
 
@@ -380,7 +406,7 @@ EXEC sp_AddEmployee 'Jonas', 'Pedersen', 'jonas@techcorp.com', '88885678', 'http
 EXEC sp_AddEmployee 'Emma', 'Christensen', 'emma@techcorp.com', '88886789', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 1, 4, 2, '5678901234', 'emma@mail.com', '56789012', 'emma', '$argon2id$v=19$m=65536,t=3,p=1$juAK/GbhT5I4VKZ6mn1oCQ$QTcNboWjI+8zzRYPpSNN+LVJJQgnIgxKmn2BEzpwJ/U', 3;
 
 
-EXEC sp_AddEmployee 'Noaah', 'Larsaen', 'noaah@techcorp.com', '88887890', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 2, 2, 1, '6789012345', 'noaah@mail.com', '67890123', 'manager', '$argon2id$v=19$m=65536,t=3,p=1$FLGTzptnSRjaNVK067W4RQ$oXnrEef/IEc9mmixH2O5f23NZTwd9oGdx4n5D8D16FI', 2;
+EXEC sp_AddEmployee 'Noaah', 'Larsaen', 'noaah@techcorp.com', '88887890', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 6, 6, 1, '6789012345', 'noaah@mail.com', '67890123', 'manager', '$argon2id$v=19$m=65536,t=3,p=1$FLGTzptnSRjaNVK067W4RQ$oXnrEef/IEc9mmixH2O5f23NZTwd9oGdx4n5D8D16FI', 2;
 
 
 EXEC sp_AddEmployee 'Freja', 'Mortensen', 'freja@techcorp.com', '88888901', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 3, 3, 2, '7890123456', 'freja@mail.com', '78901234', 'readonly', '$argon2id$v=19$m=65536,t=3,p=1$7tb9VjgW0gNU1Wum0YtREw$fEouKLvVNV7eThELz1kiG9fgVKaK/T1vjjwsIHkSmMY', 4;
@@ -389,10 +415,10 @@ EXEC sp_AddEmployee 'Freja', 'Mortensen', 'freja@techcorp.com', '88888901', 'htt
 EXEC sp_AddEmployee 'Lucas', 'Olsen', 'lucas@techcorp.com', '88889012', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 4, 4, 3, '8901234567', 'lucas@mail.com', '89012345', 'lucas', '$argon2id$v=19$m=65536,t=3,p=1$2PGgInSkQGN8cafL2KVziw$fSz9lK0zFPa9IEZxfB6H6Q9IHGk1jkghnSdXjTiGxRs', 3;
 
 
-EXEC sp_AddEmployee 'Ida', 'Thomsen', 'ida@techcorp.com', '88880123', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 1, 2, 4, '9012345678', 'ida@mail.com', '90123456', 'ida', '$argon2id$v=19$m=65536,t=3,p=1$JkK383CoP2tyS8KskUlqIg$Dg/v4jKvGP5FtyRizPX24tIj1URn490pyDBUwL2jAlU', 3;
+EXEC sp_AddEmployee 'Ida', 'Thomsen', 'ida@techcorp.com', '88880123', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 1, 5, 4, '9012345678', 'ida@mail.com', '90123456', 'ida', '$argon2id$v=19$m=65536,t=3,p=1$JkK383CoP2tyS8KskUlqIg$Dg/v4jKvGP5FtyRizPX24tIj1URn490pyDBUwL2jAlU', 3;
 
 
-EXEC sp_AddEmployee 'William', 'Rasmussen', 'william@techcorp.com', '88881235', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 2, 1, 3, '0123456789', 'william@mail.com', '01854567', 'william', '$argon2id$v=19$m=65536,t=3,p=1$Xne/F0ZfOHjenPgyCj3siA$xh4r5vtfYzMnvqiv7Mm2RU5NLAngDXddeZ3R5RZ0Nn0', 3;
+EXEC sp_AddEmployee 'William', 'Rasmussen', 'william@techcorp.com', '88881235', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 5, 1, 3, '0123456789', 'william@mail.com', '01854567', 'william', '$argon2id$v=19$m=65536,t=3,p=1$Xne/F0ZfOHjenPgyCj3siA$xh4r5vtfYzMnvqiv7Mm2RU5NLAngDXddeZ3R5RZ0Nn0', 3;
 
 
 EXEC sp_AddEmployee 'Clara', 'Hansen', 'clara@techcorp.com', '88882345', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png', 3, 4, 1, '1123456790', 'clara@mail.com', '11234567', 'clara', '$argon2id$v=19$m=65536,t=3,p=1$D7OPcfW1hQRmlXBpE69RrQ$dDrVkd4e2WrBYcoAFc9WLPlyHNl/1xUc2oTxDAzzKHw', 3;
@@ -401,26 +427,26 @@ EXEC sp_AddEmployee 'Adminn', 'Adminn', 'Adminn@techcorp.com', '88888917', 'http
 
 EXEC sp_AddEmployee 'Oliver', 'Skov', 'oliver.skov@techcorp.com', '77770001',
 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-2, 2, 2, '1234500011', 'oliver@mail.com', '70010001', 'olivers',
-'$argon2id$v=19$m=65536,t=3,p=1$example1$hash1', 2;
+2, 2, 4, '1234500011', 'oliver@mail.com', '70010001', 'olivers',
+'$argon2id$v=19$m=65536,t=3,p=1$example1$hash1', 3;
 
 
 EXEC sp_AddEmployee 'Laura', 'Hvid', 'laura.hvid@techcorp.com', '77770002',
 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-2, 2, 2, '1234500022', 'laura@mail.com', '70020002', 'laurah',
-'$argon2id$v=19$m=65536,t=3,p=1$example2$hash2', 2;
+4, 2, 2, '1234500022', 'laura@mail.com', '70020002', 'laurah',
+'$argon2id$v=19$m=65536,t=3,p=1$example2$hash2', 3;
 
 
 EXEC sp_AddEmployee 'Tobias', 'Lund', 'tobias.lund@techcorp.com', '77770003',
 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-2, 2, 2, '1234500033', 'tobias@mail.com', '70030003', 'tobiasl',
-'$argon2id$v=19$m=65536,t=3,p=1$example3$hash3', 2;
+4, 2, 2, '1234500033', 'tobias@mail.com', '70030003', 'tobiasl',
+'$argon2id$v=19$m=65536,t=3,p=1$example3$hash3', 3;
 
 
 EXEC sp_AddEmployee 'Sara', 'Dam', 'sara.dam@techcorp.com', '77770004',
 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-2, 2, 2, '1234500044', 'sara@mail.com', '70040004', 'sarad',
-'$argon2id$v=19$m=65536,t=3,p=1$example4$hash4', 2;
+4, 2, 4, '1234500044', 'sara@mail.com', '70040004', 'sarad',
+'$argon2id$v=19$m=65536,t=3,p=1$example4$hash4', 3;
 
 
 EXEC sp_AddEmployee 'Victor', 'Holm', 'victor.holm@techcorp.com', '77770005',
@@ -431,31 +457,31 @@ EXEC sp_AddEmployee 'Victor', 'Holm', 'victor.holm@techcorp.com', '77770005',
 
 EXEC sp_AddEmployee 'Julie', 'Møller', 'julie.moller@techcorp.com', '77770006',
 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-2, 2, 2, '1234500066', 'julie@mail.com', '70060006', 'juliem',
-'$argon2id$v=19$m=65536,t=3,p=1$example6$hash6', 2;
+2, 2, 4, '1234500066', 'julie@mail.com', '70060006', 'juliem',
+'$argon2id$v=19$m=65536,t=3,p=1$example6$hash6', 3;
 
 
 EXEC sp_AddEmployee 'Anders', 'Bro', 'anders.bro@techcorp.com', '77770007',
 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-2, 2, 2, '1234500077', 'anders@mail.com', '70070007', 'andersb',
-'$argon2id$v=19$m=65536,t=3,p=1$example7$hash7', 2;
+2, 2, 4, '1234500077', 'anders@mail.com', '70070007', 'andersb',
+'$argon2id$v=19$m=65536,t=3,p=1$example7$hash7', 3;
 
 
 EXEC sp_AddEmployee 'Maja', 'Frisk', 'maja.frisk@techcorp.com', '77770008',
 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-2, 2, 2, '1234500088', 'maja@mail.com', '70080008', 'majaf',
-'$argon2id$v=19$m=65536,t=3,p=1$example8$hash8', 2;
+4, 2, 2, '1234500088', 'maja@mail.com', '70080008', 'majaf',
+'$argon2id$v=19$m=65536,t=3,p=1$example8$hash8', 3;
 
 
 EXEC sp_AddEmployee 'Sebastian', 'Krag', 'sebastian.krag@techcorp.com', '77770009',
 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-2, 2, 2, '1234500099', 'sebastian@mail.com', '70090009', 'sebastiank',
+2, 2, 3, '1234500099', 'sebastian@mail.com', '70090009', 'sebastiank',
 '$argon2id$v=19$m=65536,t=3,p=1$example9$hash9', 2;
 
 
 EXEC sp_AddEmployee 'Nanna', 'Poulsen', 'nanna.poulsen@techcorp.com', '77770010',
 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-2, 2, 2, '1234500100', 'nanna@mail.com', '70100010', 'nannap',
+4, 2, 3, '1234500100', 'nanna@mail.com', '70100010', 'nannap',
 '$argon2id$v=19$m=65536,t=3,p=1$example10$hash10', 2;
 
 
@@ -492,9 +518,9 @@ INSERT INTO project_tasks (name, completed, time_of_completion, time_to_complete
 ('Develop campaign strategy', 1, '2025-09-30 15:30:00', 2, 6),
 ('Create promotional materials', 0, NULL, NULL, 6),
 ('Launch social media advertising', 0, NULL, NULL, 6),
-('Plan event logistics', 1, '2023-01-15 10:00:00', 2, 8),
+('Plan event logistics', 1, '2023-01-15 10:00:00', 3, 8),
 ('Book venue and catering', 1, '2023-01-20 14:30:00', 1, 8),
-('Execute event successfully', 1, '2023-02-22 18:00:00', 3, 8),
+('Execute event successfully', 1, '2023-02-22 18:00:00', 4, 8),
 ('Install solar panels', 0, NULL, NULL, 3);
 INSERT INTO stress_measures (description, measure, employee_id, task_id) VALUES
 ('Deadline pressure', 7, 2, 2),
@@ -516,7 +542,7 @@ INSERT INTO  project_tasks_employees (employee_id, task_id, currently_working_on
 (8, 2, 1),
 (9, 1, 1),
 (10, 4, 1),
-(7, 4, 1),
+(7, 12, 1),
 (11, 4, 1),
 (11, 7, 0),
 (12, 5, 1),
@@ -538,23 +564,78 @@ INSERT INTO  project_tasks_employees (employee_id, task_id, currently_working_on
 
 INSERT INTO employees_projects (project_id, employee_id, currently_working_on, is_project_manager) VALUES
 (1, 1, 1, 1),
-(1, 2, 1, 0),
-(2, 3, 1, 0),
-(3, 4, 0, 0),
-(2, 1, 0, 0),
-(3, 5, 1, 1),
-(3, 6, 1, 0);
-
+(1, 2, 0, 0),
+(1, 4, 1, 0),
+(1, 7, 1, 0),
+(1, 8, 1, 0),
+(1, 9, 1, 0),
+(2, 5, 1, 1),
+(2, 6, 1, 0),
+(2, 10, 1, 0),
+(2, 11, 1, 0),
+(2, 12, 1, 0),
+(2, 21, 1, 0),
+(3, 14, 1, 1),
+(3, 23, 1, 0),
+(4, 11, 0, 0),
+(4, 15, 1, 1),
+(4, 16, 1, 0),
+(4, 17, 1, 0),
+(5, 3, 0, 0),
+(5, 7, 1, 1),
+(5, 13, 0, 0),
+(5, 18, 1, 0),
+(6, 19, 1, 0),
+(6, 20, 1, 1),
+(6, 22, 1, 0),
+(8, 15, 0, 0),
+(8, 18, 0, 0),
+(8, 21, 0, 1);
 
 INSERT INTO projects_disc_profiles (project_id, disc_profile_id) VALUES
 (1, 1),
 (1, 2),
+(1, 2),
+(1, 3),
+(1, 4),
+(2, 1),
+(2, 2),
 (2, 3),
-(3, 4);
+(2, 4),
+(2, 1),
+(2, 2),
+(2, 3),
+(3, 4),
+(3, 1),
+(4, 1),
+(4, 2),
+(4, 1),
+(4, 3),
+(4, 1),
+(5, 1),
+(5, 1),
+(5, 2),
+(5, 2),
+(5, 3),
+(5, 4),
+(5, 1),
+(5, 1),
+(6, 1),
+(6, 1),
+(6, 3),
+(6, 4),
+(7, 4),
+(7, 4),
+(7, 4),
+(7, 4),
+(7, 3),
+(7, 2),
+(7, 3),
+(8, 3);
 
 PRINT 'Dummy data inserted successfully!';
 
-
+GO
 -- CREATE JOB in msdb
 USE msdb;
 GO
