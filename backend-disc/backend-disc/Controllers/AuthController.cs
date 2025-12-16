0f3f14934f82ef6945b76cb331deb9e0550b767c
olliwh <98967791+olliwh@users.noreply.github.com>
2025-12-16 13:52:03 +0100

    sonar

--- backend-disc/backend-disc/Controllers/AuthController.cs
+++ backend-disc/backend-disc/Controllers/AuthController.cs
@@ -37,7 +37,7 @@ namespace backend_disc.Controllers
 
         public async Task<IActionResult> Error([FromBody] LoginDto dto)
         {
-            throw new Exception("This is a test exception for global error handling.");
+            throw new Exception("This is a test exception for global error handling. need smal code change for sonar");
         }
     
     }
